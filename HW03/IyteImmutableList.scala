ba17088d5fbf88df9fbd67ee36560be60a23efbf
uttyc <atikcandurmus@gmail.com>
2016-11-15 14:19:08 +0400

    Update IyteImmutableList.scala

--- HW03/IyteImmutableList.scala
+++ HW03/IyteImmutableList.scala
@@ -1,18 +1,20 @@
-class IyteImmutableList{
-	var myList = List.empty[Int]
-
-	def add(item: Int): List[Int] ={
-		item :: myList
-		myList
-	}
-	override def toString() : String = {
-		var str = ""
-		for( i <- myList) {
-			str += i
-		}
-		str
-	}
+class IyteImmutableList(val head: Int, val tail: HelperList) extends HelperList {
+  override def toString: String = {
+    var str = head + "," + tail
+    if(str.endsWith(",")) str.substring(0,str.length()-1)
+    else str
+  }
 }
-object IyteImmutableList{
-	def apply():IyteImmutableList = IyteImmutableList()
+abstract class HelperList {
+  def head: Int
+  def tail: HelperList
+  def add(item: Int): HelperList = new IyteImmutableList(item, this)
+}
+object Nill extends HelperList {
+  def head: Nothing = throw new Exception("Can't index head")
+  def tail: HelperList = throw new Exception("Can't index tail")
+  override def toString =  ""
+}
+object IyteImmutableList {
+  def apply(): HelperList = Nill
 }
